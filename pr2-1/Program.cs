6bd335a48a39821eb99b44fa0cb197033afaae45
gorka1369 <Tancor@icloud.com>
2020-09-03 16:49:22 +0300

    Добавьте файлы проекта.

---
+++ pr2-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace ShowFilesDemo
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            // Показать все файлы 
+
+            foreach (FileInfo file in dir.GetFiles())
+
+            {
+                Console.WriteLine("File: {0}", file.FullName);
+            }
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                ShowDirectory(subDir);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Tanco\OneDrive\Рабочий стол\yziki"); ShowDirectory(dir);
+        }
+    }
+}
